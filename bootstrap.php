cb8e53cfaadbec62ecbd6c8d876203395a5a0614
Taylor Otwell <taylorotwell@gmail.com>
2011-07-16 10:59:55 -0500

    adjusted paths.

--- bootstrap.php
+++ bootstrap.php
@@ -3,10 +3,10 @@
 // --------------------------------------------------------------
 // Define the framework paths.
 // --------------------------------------------------------------
-define('BASE_PATH', realpath('../').'/');
-define('APP_PATH', realpath('../application').'/');
-define('SYS_PATH', realpath('../system').'/');
-define('PUBLIC_PATH', realpath('../public').'/');
+define('BASE_PATH', realpath('../laravel').'/');
+define('APP_PATH', realpath('../laravel/application').'/');
+define('SYS_PATH', realpath('../laravel/system').'/');
+define('PUBLIC_PATH', realpath('../laravel/public').'/');
 define('PACKAGE_PATH', APP_PATH.'packages/');
 
 // --------------------------------------------------------------
