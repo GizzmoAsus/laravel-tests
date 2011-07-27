8340280e2822437668cb4409a07455274497eca7
Taylor Otwell <taylorotwell@gmail.com>
2011-07-26 21:27:46 -0500

    tweaked paths and constants.

--- bootstrap.php
+++ bootstrap.php
@@ -1,20 +1,32 @@
 <?php
+/**
+ * Unit Tests For The Laravel PHP Framework.
+ *
+ * @package  Laravel
+ * @version  1.3.0
+ * @author   Taylor Otwell
+ * @link     http://laravel.com
+ */
 
 // --------------------------------------------------------------
-// Define the framework paths.
+// Define the core framework paths.
 // --------------------------------------------------------------
-define('BASE_PATH', realpath('../laravel').'/');
 define('APP_PATH', realpath('../laravel/application').'/');
+define('BASE_PATH', realpath('../laravel').'/');
+define('PUBLIC_PATH', realpath('../laravel/public').'/');
 define('SYS_PATH', realpath('../laravel/system').'/');
-define('CONFIG_PATH', APP_PATH.'config/');
-define('PACKAGE_PATH', APP_PATH.'packages/');
-define('PUBLIC_PATH', BASE_PATH.'public/');
-define('PUBLIC_PATH', BASE_PATH.'public/');
 
 // --------------------------------------------------------------
-// Define the fixture path.
+// Define various other framework paths.
 // --------------------------------------------------------------
+define('CONFIG_PATH', APP_PATH.'config/');
 define('FIXTURE_PATH', realpath('fixtures').'/');
+define('LIBRARY_PATH', APP_PATH.'libraries/');
+define('MODEL_PATH', APP_PATH.'models/');
+define('PACKAGE_PATH', APP_PATH.'packages/');
+define('ROUTE_PATH', APP_PATH.'routes/');
+define('SYS_VIEW_PATH', SYS_PATH.'views/');
+define('VIEW_PATH', APP_PATH.'views/');
 
 // --------------------------------------------------------------
 // Define the PHP file extension.
@@ -28,11 +40,29 @@ require SYS_PATH.'config'.EXT;
 require SYS_PATH.'arr'.EXT;
 
 // --------------------------------------------------------------
+// Register the auto-loader.
+// --------------------------------------------------------------
+spl_autoload_register(function($class) 
+{
+	$file = strtolower(str_replace('\\', '/', $class));
+
+	if (array_key_exists($class, $aliases = System\Config::get('aliases')))
+	{
+		return class_alias($aliases[$class], $class);
+	}
+
+	foreach (array(BASE_PATH, MODEL_PATH, LIBRARY_PATH) as $directory)
+	{
+		if (file_exists($path = $directory.$file.EXT))
+		{
+			require $path;
+
+			return;
+		}
+	}
+});
+
+// --------------------------------------------------------------
 // Load the test utilities.
 // --------------------------------------------------------------
 require 'utils'.EXT;
-
-// --------------------------------------------------------------
-// Register the auto-loader.
-// --------------------------------------------------------------
-spl_autoload_register(require SYS_PATH.'loader'.EXT);
